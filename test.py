52d56f2beeea0113174ff12d765fbba8d83b3983
JayJay <snoopy_zj@163.com>
2024-05-13 20:25:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from modeltraining import *
 
 if __name__ == "__main__":
-    test_file1="D:\\kust\\419\\Speaker_Identification_GMM\\testdata\\"
-    test_file="D:\\kust\\419\\Speaker_Identification_GMM\\testdata\\dongT.wav"
-    train_file="D:\\kust\\419\\Speaker_Identification_GMM\\trainData\\"
+    test_file1="Speaker_Identification_GMM\testdata\"
+    test_file="Speaker_Identification_GMM\testdata\dongT.wav"
+    train_file="Speaker_Identification_GMM\trainData\"
     
     model = Model()
     model.training(train_file,1)
